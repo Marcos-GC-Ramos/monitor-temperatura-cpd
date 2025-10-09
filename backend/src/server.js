4b512017d976ab4846a98031e107f423c0d798ac
Marcos-GC-Ramos <marcosgustavoramos72@gmail.com>
2025-10-09 13:58:24 -0400

    feat: criacao de funcao para criacao de tabelas

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import { swaggerDoc } from "./config/swagger.js";
-import { conectarBanco } from "./config/db.js";
+import { pool, conectarBanco } from "./config/db.js";
 import { seedAdmin } from "./utils/seedAdmin.js";
 import loginRoutes from "./routes/login.routes.js";
 import temperaturaRoutes from "./routes/temperatura.routes.js";
@@ -24,6 +24,24 @@ app.use(
 
 // Inicialização
 await conectarBanco();
+
+// 🔹 Cria tabelas se não existirem
+await pool.query(`
+  CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
+  );
+
+  CREATE TABLE IF NOT EXISTS leituras (
+    id SERIAL PRIMARY KEY,
+    temperatura REAL,
+    data TIMESTAMP DEFAULT NOW(),
+    alarme BOOLEAN
+  );
+`);
+
 await seedAdmin();
 
 app.listen(3000, () => {
