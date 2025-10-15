668873e53331fae5d67390f677ad44e07718cb33
Marcos-GC-Ramos <marcosgustavoramos72@gmail.com>
2025-10-15 11:44:38 -0400

    style: mudando o thema para dark e adicionando cor orange como principal do sistema

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="pt-BR">
+    <html lang="pt-BR" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
